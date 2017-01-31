3a5fd3ca5b289ec9a5fad3633fc1e4b7efce474b
kel-dao <dao.kellya@yahoo.com>
2017-01-31 09:14:12 -0800

    fix errors

--- battleship.js
+++ battleship.js
@@ -1,7 +1,7 @@
 var randomLoc = Math.floor(Math.random() * 5);
 var location1 = randomLoc;
-var location2 = location1 ++;
-var location3 = location2 ++;
+var location2 = location1 + 1;
+var location3 = location2 + 1;
 var guess;
 var hits = 0;
 var guesses = 0;
@@ -12,16 +12,16 @@ while (isSunk == false) {
   if (guess < 0 || guess > 6) {
     alert("Please enter a valid cell number!");
   } else {
-    guesses = guesses ++;
+    guesses = guesses + 1;
     if (guess == location1 || guess == location2 || guess == location3) {
-        hits = hits ++;
+        hits = hits + 1;
         alert("HIT!");
       
         if (hits == 3) {
           
           isSunk = true;
           
-          alert("You sunk my battleship!"); 
+          alert("You sank my battleship!"); 
         }
     } else {
       alert("MISS!");
